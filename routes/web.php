ce9ffab5073cdfc0a0333e9ce0054f57154adee7
鈴木颯駿 <soshunsuzuki@soshun-mb.local>
2019-09-16 16:14:11 +0900

    routing修正

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('hello/{msg?}',function ($msg='no message.') {
+
 $html = <<<EOF
 <html>
 <head>
 <title>Hello</title>
 <style>
 body {font-size:16px; color:#999;}
-h1 {font-size:100px; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+h1 {font-size:100px; text-align:right; color:#eee; 
+  margin:-40px 0px -50px 0px; }
 </style>
 </head>
 <body>
- <h1>Hello</h1>
- <p>This is sample page.</p>
+  <h1>Hello</h1>
+  <p>{$msg}</p>
+  <p>This is sample page.</p>
 </body>
 </html>
 EOF;
 
-Route::get('hello',function () use ($html){
   return $html;
 });
